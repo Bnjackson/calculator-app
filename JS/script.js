328b4f2a591160a0ea0bebcb4ae1e4658a786a00
Bnjackson <65509144+Bnjackson@users.noreply.github.com>
2020-10-02 19:52:34 +0100

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,23 @@
-/*Calculator-app */
+const addition = (first, second) =>  {
+    return first + second;
+};
+
+const subtraction = (first, second ) => {
+    return first - second;
+};
+
+const multiply = (first, second) => {
+    return first * second;
+};
+
+const divide = (first, second) => {
+    return first / second;
+};
+
+const num1 = 10;
+const num2 = 10;
+
+console.log(addition(num1, num2));
+console.log(subtraction(num1, num2));
+console.log(multiply(num1, num2));
+console.log(divide(num1, num2));
